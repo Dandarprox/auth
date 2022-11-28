6a67c41877eb30b3bf61e70831829ab8f05c0b82
facuellarg <32692084+facuellarg@users.noreply.github.com>
2022-11-28 14:15:01 -0500

    algunos cambios pequenios que no se han probado

--- service/interfaces/jwt.go
+++ service/interfaces/jwt.go
@@ -5,5 +5,5 @@ import "github.com/Dandarprox/golang-auth/entities"
 type JwtI interface {
 	GenerateToken(user *entities.User) string
 	ValidateToken(token string) (bool, error)
-	GetTokenUser(token string) (*entities.User, error)
+	GetUserDataFromToken(token string) (*entities.User, error)
 }
